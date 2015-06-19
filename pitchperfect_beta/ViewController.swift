870199b8b8b23580cc60f4b17e899e1cd11479e0
David Stroud <david@greymatter-ag.com>
2015-06-19 14:28:28 -0500

    first commit to beta project

---
+++ pitchperfect_beta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchperfect_beta
+//
+//  Created by DAVID STROUD on 6/19/15.
+//  Copyright © 2015 Grey Matter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
