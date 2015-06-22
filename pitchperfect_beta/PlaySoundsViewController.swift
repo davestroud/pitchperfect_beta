2f066f4ebdfa348fc1f3de670e8e86fafc116692
David Stroud <david@greymatter-ag.com>
2015-06-22 12:45:34 -0500

    added PlaySoundsViewController

---
+++ pitchperfect_beta/PlaySoundsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlaySoundsViewController.swift
+//  pitchperfect_beta
+//
+//  Created by DAVID STROUD on 6/22/15.
+//  Copyright © 2015 Grey Matter. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    @IBAction func talk_slow(sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
